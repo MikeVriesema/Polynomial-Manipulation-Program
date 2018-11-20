05968729b1e6ccba0885fb2219c5412512b5521d
NameJeff <37027239+CommanderDolphin@users.noreply.github.com>
2018-11-20 22:55:32 +0000

    Updated main template
    -Added main comments
    -Adjusted functions
    -Removed junk code

--- mainProgram.c
+++ mainProgram.c
@@ -1,11 +1,17 @@
 /*///////////////////////////////////////////////////////////////////////
- * Assignment 2 CE4703
+ * Assignment 3 CE4703
+ * mainProgram.c
  * 
- * Modular Program that runs shape related size calculations for
- * 3D and 2D shapes with a menu loop in the main allowing for user input.
+ * Modular Program that uses ADT to carry out various functions on 
+ * polynomial values
  * 
- * Author: Mike Vriesema 17212359
- * Date Last Modified: 19.11.2018
+ * Authors: Mike Vriesema 17212359 
+ * 			Luke O'Sullivan Griffin 17184614 
+ * 			Ryan O'Connor 17209382 
+ * 			Conall McAteer 18173586 
+ * 			Gearoid Kirwan 17213266
+ * 
+ * Date Last Modified: 20.11.2018
  *///////////////////////////////////////////////////////////////////////
 
 #include <stdlib.h>
@@ -13,57 +19,44 @@
 #include "polynomial.h"
 
 //FUNCTION DECLARATIONS:
-int readChoice();
-double readInput();
+int readOrder();
+double readCoeff();
 
 /*
  * The main 
 */
-int main()
- {
-	int t1,t2,t3;
-	struct poly p1[10],p2[10],p3[10];
-	/* read and display first polynomial */
-	t1=readPoly(p1);
-	printf(" \n First polynomial : ");
-	displayPoly(p1,t1);
-	/* read and display second polynomial */
-	t2=readPoly(p2);
-	printf(" \n Second polynomial : ");
-	displayPoly(p2,t2);
+int main(){
 
-	/* add two polynomials and display resultant polynomial */
-	t3=addPoly(p1,p2,t1,t2,p3);
-	printf(" \n\n Resultant polynomial after addition : ");
-	displayPoly(p3,t3);
-	printf("\n");
 
-	return 0;
- }
+
+
+
+
+
+	
+}
 
 /*
- * readChoice
+ * readOrder
  * Reads user input expecting an int
- * Used for making menu choices
+ * Used for taking in the order of the polynomials
  * Return: int - value(user inputted value)
  * Parameter: void
 */
-int readChoice()
-{
+int readOrder(){
    int value;
    scanf("%d",&value);
    return value;
 }
 
 /*
- * readInput
+ * readCoeff
  * Reads user input expecting an double
- * Used for inputting shape dimensions in sub menus
+ * Used for coeeficients in polynomials
  * Return: double - value(user inputted value)
  * Parameter: void
 */
-double readInput()
-{
+double readCoeff(){
    double value;
    scanf("%lf",&value);
    return value;
